3f37dfb6a66b29c18ce091ed5300bb27cf3adb70
Jayant Sharma <sharma.jayant40@gmail.com>
2020-06-29 19:05:37 +0530

    edit 2

---
+++ js/dashboard-logic.js
@@ -0,0 +1,12 @@
+var arr=new Array();
+arr[0]="https://bookscollections.s3.ap-south-1.amazonaws.com/The-Shadow-Out-of-Time.pdf";
+arr[1]="https://bookscollections.s3.ap-south-1.amazonaws.com/CloudComputing.pdf";
+arr[2]="https://bookscollections.s3.ap-south-1.amazonaws.com/Eat%2C+Pray%2C+Love+(+PDFDrive.com+).pdf";
+arr[3]="https://bookscollections.s3.ap-south-1.amazonaws.com/Rich+Dad+Poor+Dad+for+Teens_+The+Secrets+about+Money--That+You+Don't+Learn+in+School!+(+PDFDrive.com+).pdf";
+arr[4]="https://bookscollections.s3.ap-south-1.amazonaws.com/The+Power+of+Now_+A+Guide+to+Spiritual+Enlightenment+(+PDFDrive.com+).pdf";
+arr[5]="https://bookscollections.s3.ap-south-1.amazonaws.com/The-Adventures-of-Sherlock-Holmes.pdf";
+arr[6]="https://bookscollections.s3.ap-south-1.amazonaws.com/The-Invisible-Man.pdf";
+arr[7]="https://bookscollections.s3.ap-south-1.amazonaws.com/The-Mysterious-Island.pdf";
+arr[8]="https://bookscollections.s3.ap-south-1.amazonaws.com/The-Shadow-Out-of-Time.pdf";
+
+
